ffcad0ad8fa9d6a2b733d4011da3324b328a0420
Mridul <mriduljoly2000@gmail.com>
2023-02-12 12:13:47 +0530

    Searchbar

---
+++ Frondend/curriculum/src/components/SearchBar/Searchtab.jsx
@@ -0,0 +1,37 @@
+import {React, useState} from 'react'
+import '../SearchBar/Style.css';
+const Searchtab = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  return (
+    <div className="templateContainer">
+          <div className="searchInput_Container">
+            <input id="searchInput" type="text" placeholder="Search here..." onChange={(event) => {
+              setSearchTerm(event.target.value);
+            }} />
+          </div>
+          <div className="template_Container">
+            {
+              data 
+                .filter((val) => {
+                  if(searchTerm == ""){
+                    return val;
+                  }else if(val.title.toLowerCase().includes(searchTerm.toLowerCase())){
+                    return val;
+                  }
+                })
+                .map((val) => {
+                  return(
+                    <div className="template" key={val.id}>
+                        <img src={val.image} alt="" />
+                        <h3>{val.title}</h3>
+                        
+                    </div> 
+                  )
+                })
+            }
+          </div>
+        </div>
+  )
+}
+
+export default Searchtab
